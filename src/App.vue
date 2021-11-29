c5d8a932478c7f296b6c29a8b6d4beaaacd39bb9
Danny Torres <danny.torresxd@gmail.com>
2021-11-28 22:12:04 -0500

    Adding Basic routing to main pages

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,62 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar app color="primary" dark> </v-app-bar>
+    <v-navigation-drawer permanent expand-on-hover>
+      <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img
+              src="https://randomuser.me/api/portraits/women/85.jpg"
+            ></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="text-h6">
+              Sandra Adams
+            </v-list-item-title>
+            <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <router-link to="/">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Go to Home </v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link to="/products">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-food</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Go to products</v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link to="/orders">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-basket</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Go to orders </v-list-item-title>
+          </v-list-item>
+        </router-link>
+        <router-link to="/transporters">
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title> Go to transporters </v-list-item-title>
+          </v-list-item>
+        </router-link>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <router-view />
     </v-main>
